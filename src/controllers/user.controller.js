0db7865ea0fbd9c8e31559ab01ca726159dd2fe4
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-02-10 22:55:43 +0530

    update user.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,29 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/AppError.js";
+import { User } from "../models/user.model.js";
+const ragisterUser = asyncHandler(async (req, res) => {
+  // get user details from frontend
+  // validation - not empty
+  // if user already exisrs
+  //check for images , check for avatar
+  // upload them to cloudinary, avatar
+  // create user objects - create entry in db
+  // remove password and refresh token field from response
+  // return res
 
-const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
+  const { fullName, email, username, password } = req.body;
+  console.log("emails: ", email);
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
   });
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
 });
 
-export { registerUser };
+export { ragisterUser };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { ragisterUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.post("/register", registerUser);
+router.route("/ragister").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  ragisterUser
+);
 
 export default router;
